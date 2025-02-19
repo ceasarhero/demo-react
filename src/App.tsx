4bfd02bad0a893115ff3488f63b7ec525bf0dc76
Sumit Shahi <sumitshahi.dev@gamil.com>
2025-02-19 21:28:52 +0545

    Changed text of the main App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,9 @@ import Showgraph from './components/graphcompo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Buttoncompo textForButton='This is a custom Button'/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <h1>Learn with summit</h1>
-        </a>
-        Lorem ipsum cati, atque dolor quam at voluptatum facere, cum doloribus pariatur necessitatibus us nesciunt autem officiis sapiente? Libero modi debitis quisquam tenetur.
-        <Showgraph textsForParagraph=''/>
-        <Buttoncompo  textForButton='Never Click Here'/>
-      </header>
-    </div>
-  );
+  <>
+  <h1>Hello from React Ts App</h1>
+  </>
+  )
 }
-
 export default App;
