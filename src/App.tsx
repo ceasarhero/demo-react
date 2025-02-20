c9840a710894959f01c66d72181166ccc558c950
Sumit Shahi <sumitshahi.dev@gamil.com>
2025-02-20 20:49:14 +0545

    added newline at the top of the app function

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Showgraph from './components/graphcompo';
 function App() {
   return (
   <>
+  <h1>Hello, welcome to my official web page</h1>
   <h1>Hello from React Ts App</h1>
   </>
   );
